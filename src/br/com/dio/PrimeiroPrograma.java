778d7585308d387b74f458939a0178862634395b
tiagorodoliv <tiago.rod.oliv@gmail.com>
2021-11-17 20:12:09 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 10;
+        System.out.println("Hello world!!" + (a+b));*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+        System.out.println(gato);
+        System.out.println(gato);
+    }
+}
